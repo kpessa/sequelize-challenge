0fc6f50435b4f8b73d6ac8f4eec81caa1f78404d
Kurt Pessa <kpessa@gmail.com>
2021-03-16 08:16:58 -0400

    initial commit and successfully filled in columns, seeded database!

---
+++ models/index.js
@@ -0,0 +1,44 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category);
+// Categories have many Products
+Category.hasMany(Product);
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+//! Not sure if I need
+// ProductTag.belongsTo(Product, {
+//   foreignKey: 'product_id',
+// });
+
+// ProductTag.belongsTo(Tag, {
+//   foreignKey: 'tag_id',
+// });
+
+// Product.hasMany(Tag, {
+//   foreignKey: 'tag_id',
+// });
+
+// Tag.hasMany(Product, {
+//   foreignKey: 'product_id',
+// });
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
